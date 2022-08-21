ddab02ceedc56b5a1db8826c26df347dd8dad911
vizidane <quangtran1188@gmail.com>
2022-08-21 20:26:46 +0700

    greeting with text
    greeting with text

--- app/src/main/java/com/example/firstproject/MainActivity.kt
+++ app/src/main/java/com/example/firstproject/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.firstproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.firstproject.ui.theme.FirstProjectTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +25,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    //Greeting("Android")
+                    GreedingWithText("happy birthday !!!", "Thanh Thao")
                 }
             }
         }
@@ -34,10 +38,35 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+
+
+@Composable
+fun GreedingWithText(message: String, from: String) {
+    Column {
+        Text(
+            text = message,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(align = Alignment.Start)
+                .padding(start = 16.dp, end = 16.dp)
+        )
+        Text(
+            text = from,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(align = Alignment.End)
+                .padding(start = 16.dp, end = 16.dp)
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     FirstProjectTheme {
-        Greeting("Android")
+        //Greeting("Android")
+        GreedingWithText("happy birthday", "Thanh Thao")
     }
 }
